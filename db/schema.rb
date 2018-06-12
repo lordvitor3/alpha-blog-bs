04dd4bcb01e563d7523be4efd03b664594c989c1
Vitor <lordvitor3@hotmail.com>
2018-06-12 14:52:29 -0300

    alguns testes adicionados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_012550) do
+ActiveRecord::Schema.define(version: 2018_06_12_171323) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2018_06_11_012550) do
   create_table "articles2", force: :cascade do |t|
     t.string "title"
     t.text "description"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
