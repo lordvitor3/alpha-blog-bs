345d869b8c338a211104d3f928af9361434a45bf
Vitor <lordvitor3@hotmail.com>
2018-06-12 18:07:49 -0300

    mais testes e restricão de admim para categorias

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 	
 	def setup
 		@category = Category.create(name: "games")
+		@user = User.create(username: "vitor", email: "cid.vitor@hotmail.com", password: "password", admin: true)
 	end	
 
 	test "should get categories index" do 
@@ -12,10 +13,19 @@ class CategoriesControllerTest < ActionController::TestCase
 	end
 
 	test "should get new" do
+		session[:user_id] = @user.id
 		get :new
 		assert_response :success
 	end
+
 	
+	test "should redirect creat when admin not logged in" do
+		assert_no_difference 'Category.count' do
+			post :create, category: {name: "sports" }
+		end	
+		assert_redirected_to categories_path
+	end
+
 	#test "should get show" do
 	#get(:show, {'id' => @category.id})
 	#assert_response :success 
