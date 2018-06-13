780534576568e6d193215b36774ea1cb3acd3f63
Vitor <lordvitor3@hotmail.com>
2018-06-13 10:14:22 -0300

    ultima versão adicionada

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -80,6 +80,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 end
